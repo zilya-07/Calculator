167108abd0b7ed6ac999074b21d2ea772260a1c2
zilya-07 <61091984+zilya-07@users.noreply.github.com>
2024-04-24 18:34:32 +0300

    Add files via upload

---
+++ generated/source/buildConfig/debug/io/github/hanihashemi/fancycalculator/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package io.github.hanihashemi.fancycalculator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "io.github.hanihashemi.fancycalculator";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
